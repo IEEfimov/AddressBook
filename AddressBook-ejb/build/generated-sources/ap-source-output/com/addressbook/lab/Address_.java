5b96f8f7b861096474c23a0ab0ac076b23bb0ef6
GoncharenkoDanilo <goncharenko.danilo@gmail.com>
2018-09-24 16:25:05 +0300

    asshole

---
+++ AddressBook-ejb/build/generated-sources/ap-source-output/com/addressbook/lab/Address_.java
@@ -0,0 +1,18 @@
+package com.addressbook.lab;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-24T16:09:43")
+@StaticMetamodel(Address.class)
+public class Address_ { 
+
+    public static volatile SingularAttribute<Address, String> firstname;
+    public static volatile SingularAttribute<Address, String> phone;
+    public static volatile SingularAttribute<Address, Integer> groupid;
+    public static volatile SingularAttribute<Address, Integer> id;
+    public static volatile SingularAttribute<Address, String> email;
+    public static volatile SingularAttribute<Address, String> lastname;
+
+}
---
+++ AddressBook-ejb/build/generated-sources/ap-source-output/com/addressbook/lab/Groups_.java
@@ -0,0 +1,14 @@
+package com.addressbook.lab;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-24T16:09:43")
+@StaticMetamodel(Groups.class)
+public class Groups_ { 
+
+    public static volatile SingularAttribute<Groups, String> name;
+    public static volatile SingularAttribute<Groups, Integer> id;
+
+}
